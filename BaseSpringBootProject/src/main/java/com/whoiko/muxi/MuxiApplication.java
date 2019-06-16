2e4992c09b9c18f6c326529de3bc3f0bb561d7fc
vito.chen <vitochan617@gmail.com>
2019-06-16 23:39:08 +0800

    Add BaseSpringBootProject and .gitignore file.

---
+++ BaseSpringBootProject/src/main/java/com/whoiko/muxi/MuxiApplication.java
@@ -0,0 +1,13 @@
+package com.whoiko.muxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuxiApplication.class, args);
+    }
+
+}
